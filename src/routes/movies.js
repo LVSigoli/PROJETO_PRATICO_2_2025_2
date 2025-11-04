0098545c733cf4bc6ee0e2932af1621f2a909016
LVSigoli <lucasviniciussigoli@gmail.com>
2025-11-04 11:08:34 -0300

    feat: Implement find one and delete movies routes

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -29,13 +29,98 @@ const db = require("../dataBase");
  *       500:
  *         description: Erro ao buscar filmes
  */
-
 router.get("/", async (req, res) => {
   try {
     const result = await db.query(
       "SELECT * FROM filmes WHERE deleted_at IS NULL"
     );
     res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+/**
+ * @swagger
+ * /movies/{id}:
+ *   get:
+ *     summary: Retorna um filme específico pelo ID
+ *     tags: [Movies]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do filme
+ *     responses:
+ *       200:
+ *         description: Filme encontrado
+ *        400:
+ *        description: parametro Id é obrigatório
+ *       404:
+ *         description: Filme não encontrado
+ *       500:
+ *         description: Erro no servidor
+ */
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) return res.status(400).json({ message: "id é obrigatório" });
+
+    const result = await db.query(
+      "SELECT * FROM filmes WHERE id=$1 AND deleted_at IS NULL",
+      [id]
+    );
+
+    if (!result.rows.length) {
+      return res.status(404).json({ message: "Filme não encontrado" });
+    }
+
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+/**
+ * @swagger
+ * /movies/{id}:
+ *   delete:
+ *     summary: Remove um filme específico
+ *     tags: [Movies]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID do filme
+ *     responses:
+ *       200:
+ *         description: Filme removido com sucesso
+ *       404:
+ *         description: Filme não encontrado
+ *       500:
+ *         description: Erro no servidor
+ */
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) return res.status(400).json({ message: "Id é obrigatório" });
+
+    const result = await db.query(
+      "UPDATE deleted_at = NOW() FROM filmes WHERE id =$1",
+      [id]
+    );
+
+    if (result.rowCount === 0)
+      return res.status(404).json({ message: "Filme não encontrado" });
+
+    res.status(200).json({ message: "Filme removido", movie: result.rows[0] });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
